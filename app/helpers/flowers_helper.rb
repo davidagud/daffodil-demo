47ce7619e83021ccef0e5716063a4555ecf596cc
David Agudelo <davidagud@gmail.com>
2019-01-15 14:43:17 -0600

    Finished accordion buttons

--- app/helpers/flowers_helper.rb
+++ app/helpers/flowers_helper.rb
@@ -1,6 +1,9 @@
 module FlowersHelper
 
   def options_for_flowers
+
+    @masterflowers = Masterflower.all
+
       masterflower_choices = []
       @masterflowers.each do |masterflower|
         masterflower_choices.push(masterflower.masterflower_name)
