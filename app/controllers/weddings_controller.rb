e05be399ae28691595906ce8f96915e0463d2f9d
Davidagud <davidagud@gmail.com>
2019-03-31 12:44:20 -0500

    adding export button to wedding

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -149,7 +149,11 @@ class WeddingsController < ApplicationController
   def export_wedding
     @wedding = Wedding.find(params[:id])
 
-    wedding_path(:id => @wedding.id, format: "xlsx")
+    respond_to do |format|
+      wedding_name = @wedding.wedding_name
+      format.html
+      format.xlsx { response.headers['Content-Disposition'] = 'attachment; filename = ' + @wedding.wedding_name + '.xlsx' }
+    end
   end
 
   def destroy
