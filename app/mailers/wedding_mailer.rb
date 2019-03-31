ecc07d3d6441dfaa54cfcb65b44ef934f97ebeee
Davidagud <davidagud@gmail.com>
2019-03-31 12:05:08 -0500

    adding export button to wedding

--- app/mailers/wedding_mailer.rb
+++ app/mailers/wedding_mailer.rb
@@ -3,9 +3,9 @@ class WeddingMailer < ApplicationMailer
   def wedding_email
     @wedding = Wedding.find(params[:wedding].id)
 
-    xlsx = render layout: false, handlers: [:axlsx], formats: [:xlsx], template: "weddings/export", locals: { :wedding => @wedding }
+    xlsx = render layout: false, handlers: [:axlsx], formats: [:xlsx], template: "weddings/show", locals: { :wedding => @wedding }
     attachments[@wedding.wedding_name + '.xlsx'] = {mime_type: Mime[:xlsx], content: xlsx}
-    mail(to: 'daffodilparkerbackup@gmail.com', subject: @wedding.wedding_name)
+    mail(to: 'davidagud@gmail.com', subject: @wedding.wedding_name)
   end
 
 end
