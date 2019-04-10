f669c5d51d8fb4668d5f8f092c682caea6efd7c9
Davidagud <davidagud@gmail.com>
2019-04-09 19:08:07 -0500

    putting recipes on individual pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -64,4 +64,16 @@ class RecipesController < ApplicationController
       params.require(:recipe).permit(:recipe_name, :recipe_ingredient, :recipe_quantity, :retail_price, :notes)
     end
 
+    def flower_total_price
+      @flower.flower_total_price = @flower.quantity * @flower.flower_price
+    end
+
+    def hard_good_total_price
+      @hard_good.hard_good_total_price = @hard_good.hard_good_quantity * @hard_good.hard_good_price
+    end
+
+    def recipe_total_price
+      @recipe.recipe_total_price = @recipe.recipe_quantity * (@recipe.flowers.sum(&:flower_total_price) + @recipe.hard_goods.sum(&:hard_good_total_price))
+    end
+
 end
