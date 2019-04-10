7f5a6008afd0503506aa8607e628e39764e058e8
Davidagud <davidagud@gmail.com>
2019-04-10 07:26:57 -0500

    putting recipes on individual pages

--- app/helpers/flowers_helper.rb
+++ app/helpers/flowers_helper.rb
@@ -4,7 +4,7 @@ module FlowersHelper
 
     @masterflowers = Masterflower.all
 
-      masterflower_choices = [['Select One', ' ']]
+      masterflower_choices = []
       @masterflowers.each do |masterflower|
         masterflower_choices.push(masterflower.masterflower_name + " - " + masterflower.vendor)
       end
