db966d9814db23602d85a57d6c4a0e395835e246
Davidagud <davidagud@gmail.com>
2019-04-17 21:49:29 -0500

    fixing update all buttons

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -183,7 +183,7 @@ class WeddingsController < ApplicationController
 
     def find_name(flower_name)
       @masterflowers = Masterflower.all
-      @masterflowers.where("masterflower_name ilike ?", "#{flower_name}")
+      @masterflowers.where("masterflower_name || ' - ' || vendor ilike ?", "#{flower_name}")
     end
 
 end
