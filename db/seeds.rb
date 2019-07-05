fdb32b77dff0bdcf189b7c8763bf4a6b71803751
davidagud <davidagud@gmail.com>
2019-07-05 14:13:26 -0500

    added seeds for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(username: 'demo-user', password_digest: User.digest('demo-password') );
+User.create!(username: 'demo-user', password_digest: BCrypt::Password.create('Demo-password1') );
