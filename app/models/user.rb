42bd30c920e25c8263363fe6ad422c8d7fbe302a
davidagud <davidagud@gmail.com>
2019-07-05 13:48:39 -0500

    added seeds for demo

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,10 @@ class User < ApplicationRecord
     errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
   end
 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
